7d061a51ff4231343c9c72aa32b9ae96a23e7476
AariyanApu <aariyan.apu@gmail.com>
2022-02-11 13:06:37 +0600

    faild first time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <h1>Header From App</h1>
-    </div>
-  );
-}
+
+  
+
+  useEffect(() => {
+    fetch("http://www.omdbapi.com/?s=man&apikey=a2d3eef5")
+      .then((res) => res.json())
+
+  }, []);
+
+
+    return (
+      <ul>
+        {items.map((Search) => (
+          <li>{Search[2].Title}</li>
+        ))}
+      </ul>
+    );
+  }
+
 
 export default App;
